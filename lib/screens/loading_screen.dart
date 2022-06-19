80fe6ac8837a896b76d08f8902c16c8ca31909c4
ASUS <eb.mehrdad@gmail.com>
2022-06-19 11:41:24 +0430

    api modified

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import '/services/location.dart';
+import 'dart:convert';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,17 +18,34 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      print(position);
-    } catch (e) {
-      print(e);
+    Location location = Location();
+    await location.getLocation();
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+
+
+      var longtitude = json.decode(data)['coord']['lat'];
+      var latitude = json.decode(data)['coord']['lon'];
+      var weatherDescription = json.decode(data)['weather'][0]['main'];
+      var condition = json.decode(data)['weather'][0]['id'];
+      var cityName = json.decode(data)['name'];
+
+
+    } else {
+      print(response.statusCode);
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
---
+++ lib/services/location.dart
@@ -0,0 +1,25 @@
+
+
+    
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+
+class Location {
+  double latitude;
+  double longitude;
+
+  Future<void> getLocation() async {
+    try {
+      Position position = await Geolocator
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
